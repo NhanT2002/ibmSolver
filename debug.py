4151b8a60d6d6c69a844e68d5b3e86bf78555459
Nhan <hieu-nhan.tran@polymtl.ca>
2025-10-30 15:54:51 -0400

    clean repo

--- debug.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-data = pd.read_csv('debug.txt', sep=", ", header=None)
-x = data[0]
-y = data[1]
-plt.figure()
-plt.plot(x, y, "o")
